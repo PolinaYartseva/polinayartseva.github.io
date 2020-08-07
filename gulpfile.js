e335e8f054c4b45fddb331d998e95199f99c9b5d
PolinaYartseva <polina_yartseva@hotmail.com>
2020-08-08 00:52:47 +0200

    1st commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const watch = require('gulp-watch');
+//const browserSync = require( 'browser-sync' ).create();
+
+// Styles
+gulp.task('style', function() {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ style: 'expanded', }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('./css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Watch
+gulp.task('watch', function() {
+
+    // Listen on port 35729
+    server.listen(35729, function (err) {
+        if (err) {
+            return console.log(err)
+        };
+
+        // Watch .scss files
+        gulp.watch('src/styles/**/*.scss', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            gulp.run('styles');
+        });
+
+        // Watch .js files
+        gulp.watch('src/scripts/**/*.js', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            gulp.run('scripts');
+        });
+
+        // Watch image files
+        gulp.watch('src/images/**/*', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            gulp.run('images');
+        });
+
+    });
+
+});
